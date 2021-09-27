1fc21dac9d0ad5a181597eeaec2964ec73002589
honcur <honcur@163.com>
2021-09-27 11:35:25 +0800

    code refactoring

--- contracts/proxy/CustomizedProxy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-
-abstract contract CustomizedProxy is TransparentUpgradeableProxy{
-
-}
