0ea4a287480fe02655ed8734a853635a65880c4d
honcur <honcur@163.com>
2022-03-20 12:02:20 +0800

    add DcoinPricePrediction script

---
+++ scripts/deploy-DCoinPricePrediction.ts
@@ -0,0 +1,36 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers} from 'hardhat'
+const nftFactoryAddress = "0x4566Cf31B204985259aDb368337D9C8d1ec92E96";
+const dcoinAddress = "0x079c29b4f37CEF7DDF6eC68A8BaC48A220eb72Bf";
+const bnbOracle = "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526";
+const admin = "0x9e59Ba0D8a31094e714614Fd456e9a6ABa6925fA";
+import { BigNumber } from 'ethers'
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const DCoinPricePrediction = await ethers.getContractFactory("DCoinPricePrediction");
+   let dfx = BigNumber.from("10000000000000000")
+  const dcoinPricePrediction = await DCoinPricePrediction.deploy();
+  await dcoinPricePrediction.deployed();
+  await dcoinPricePrediction.initialize(dcoinAddress, bnbOracle,admin, admin, 100 ,20, 10, 300, nftFactoryAddress );
+  console.log("Greeter deployed to:", dcoinPricePrediction.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
